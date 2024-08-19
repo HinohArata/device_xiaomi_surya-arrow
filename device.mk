b114685592d9ca7d13bd0902d553b1085d8bedc3
HinohArata <mmgcntk@gmail.com>
2024-08-19 07:34:15 +0000

    Bringup to ArrowOS [2/n]

--- device.mk
+++ device.mk
@@ -286,8 +286,8 @@ PRODUCT_PACKAGES += \
     vendor.lineage.health-service.default
 
 # LiveDisplay
-PRODUCT_PACKAGES += \
-    vendor.lineage.livedisplay@2.1-service.surya
+#PRODUCT_PACKAGES += \
+#    vendor.lineage.livedisplay@2.1-service.surya
 
 # Media
 PRODUCT_PACKAGES += \
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,7 +5,7 @@ git clone https://github.com/HinohArata/android_hardware_xiaomi.git -b lineage-2
 git clone https://github.com/HinohArata/surya_vendor.git -b lineage-20 vendor/xiaomi/surya
 
 # Clone Repository for Kernel
-git clone https://github.com/Shiryu-Kernel/kernel_xiaomi_surya.git -b 4.14 kernel/xiaomi/surya
+git clone --depth=1 https://github.com/Shiryu-Kernel/kernel_xiaomi_surya.git -b 4.14 kernel/xiaomi/surya
 
 # Clone Repository for Firmware
 git clone https://gitlab.com/HinohArata/firmware_xiaomi_surya.git firmware/xiaomi/surya
